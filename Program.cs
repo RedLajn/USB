0347e2b8fab68100d4060ae00ccd05f5853d13d1
RedLajn <119898137+RedLajn@users.noreply.github.com>
2024-12-05 14:56:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Management;
+
+class Program
+{
+    static void Main()
+    {
+        var insertWatcher = new ManagementEventWatcher();
+        insertWatcher.Query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2");
+        insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+        insertWatcher.Start();
+
+        var removeWatcher = new ManagementEventWatcher();
+        removeWatcher.Query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 3");
+        removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+        removeWatcher.Start();
+
+        Console.WriteLine("Monitoring USB connections. Press Enter to exit.");
+        Console.ReadLine();
+
+        insertWatcher.Stop();
+        removeWatcher.Stop();
+    }
+
+    private static void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+    {
+        Console.WriteLine("USB device inserted.");
+    }
+
+    private static void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+    {
+        Console.WriteLine("USB device removed.");
+    }
+}
